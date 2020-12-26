cbb98b8c25175c596363ceda1c8a771ca6952504
Cogino <67237215+Haletran@users.noreply.github.com>
2020-12-26 21:57:52 +0100

    Update auto.py

--- auto.py
+++ auto.py
@@ -5,20 +5,10 @@ import subprocess
 import schedule
 import time
 
-day = datetime.datetime.today().weekday()
-hour = datetime.datetime.now().hour
-drive = "https://drive.google.com/drive/folders/1Pw3IrUWfMep7SzaudP9mUhFDaL4ttfhf"
-url1 = "https://univ-poitiers.webex.com/meet/sanja.boskovic"
-url2 = "https://univ-poitiers.webex.com/meet/irina.bondareva"
-url3 = "https://univ-poitiers.webex.com/meet/olga.osinovskaya"
-url4 = "https://univ-poitiers.webex.com/meet/raluca.nita"
-url5 = "https://univ-poitiers.webex.com/meet/andrew.peterson"
-url6 = "https://univ-poitiers.webex.com/meet/anita.jorge"
-url7 = "https://univ-poitiers.webex.com/meet/nicolas.cormeau"
-url8 = "https://univ-poitiers.webex.com/univ-poitiers/j.php?MTID=m5cc9bbc014e051be70b488afa018712a"
-url9 = "https://univ-poitiers.webex.com/univ-poitiers/j.php?MTID=m17b56450e300259c9f9b70d4d98d97dc"
-url10 = "https://univ-poitiers.webex.com/meet/alix.tubman"
-drive = "https://drive.google.com/drive/folders/1Pw3IrUWfMep7SzaudP9mUhFDaL4ttfhf"
+"""Add your webex link"""
+url1 = "YOUR WEBEX LINK ..."
+url2 = "YOUR WEBEX LINK ..."
+
 
 """Instructions to launch WEBEX link"""
 
@@ -34,12 +24,14 @@ def lrm():
 
 
 """Schedule classes at a certain day and time"""
-russe = schedule.every().monday.at("10:00")
-informatique = schedule.every().tuesday.at("10:00")
-entreprise = schedule.every().wednesday.at("10:00")
-anglais = schedule.every().thursday.at("10:00").do(lrm)
-francais = schedule.every().saturday.at("21:46").do(rcr)
+russe = schedule.every().monday.at("10:00").do(rcr)
+informatique = schedule.every().tuesday.at("10:00").do(lrm)
+entreprise = schedule.every().wednesday.at("10:00").do()
+anglais = schedule.every().thursday.at("10:00").do()
+francais = schedule.every().saturday.at("10:00").do()
+
 
 while True:
     schedule.run_pending()
     time.sleep(1)
+
